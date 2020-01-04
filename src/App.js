501251e2dbc6000d049f183839415af3a4133516
nk6232 <nk6232@att.com>
2020-01-03 21:51:12 -0800

    [dedbeetz] attribute

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          It's Zinger!
+          It's Zinger and Flame!
         </p>
         <a
           className="App-link"
